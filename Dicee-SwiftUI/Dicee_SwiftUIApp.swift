115e242d87876b92626986eb35154ba9ae4a9fdb
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-02 07:28:47 +0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Taha Enes Aslantürk on 2.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
